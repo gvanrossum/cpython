3792bc7260dce8a1214716209aae2140a92d66b8
Guido van Rossum <guido@python.org>
2021-06-13 10:28:30 -0700

    Make the test a unittest.TestCase

--- Lib/test/test_new_pyc.py
+++ Lib/test/test_new_pyc.py
@@ -2,6 +2,8 @@
 
 import dis
 import marshal
+import unittest
+
 from test import test_tools
 
 test_tools.skip_if_missing("pyco")
@@ -10,24 +12,27 @@ with test_tools.imports_under_tool("pyco"):
     import updis
 
 
-# TODO: Turn this into a TestCase
-def main():
-    source = "a = None"
-    builder = pyco.build_source(source)
-    pyco.report(builder)
-    data = pyco.serialize_source(source)
-    print(repr(data))
-    code = marshal.loads(data)
-    # code = compile(source, "", "exec")
-    print(code)
-    print(code.co_code)
-    dis.dis(code.co_code)
-    ns = {}
-    exec(code, ns)
-    a = ns["a"]
-    assert a is None
-    print("Done: a =", a)
+class TestNewPyc(unittest.TestCase):
+    def test_basic(self):
+        value = (1, 2, 3, 3.14, 1+2j, "ab", b"cd", None, False, True, ...)
+        source = f"a = {value!r}"
+        builder = pyco.build_source(source)
+        pyco.report(builder)
+        data = pyco.serialize_source(source)
+        print(repr(data))
+        code = marshal.loads(data)
+        # code = compile(source, "", "exec")
+        print(code)
+        print(code.co_code)
+        dis.dis(code.co_code)
+        ns = {}
+        exec(code, ns)
+        a = ns["a"]
+        assert a == value, (a, value)
+        for x, y in zip(a, value):
+            assert type(x) is type(y), (x, y)
+        print("Done: a =", a)
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
