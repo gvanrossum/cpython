49369c636891ab5ba0dd8e605fcef7a077273660
Irit Katriel <iritkatriel@yahoo.com>
2021-06-29 23:21:20 +0100

    add test that calls the functions

--- Lib/test/test_new_pyc.py
+++ Lib/test/test_new_pyc.py
@@ -75,11 +75,16 @@ class TestNewPyc(unittest.TestCase):
         assert f(1, 10) == 11
         assert f(a=1, b=10) == 11
 
-    def do_test_speed(self, body, test_name):
+    def do_test_speed(self, body, test_name, call=False):
         functions = [
             f"def f{num}(a, b):\n{body}"
             for num in range(100)
         ]
+        if call:
+            functions.extend([
+                f"\nf{num}({num}, {num+1})\n"
+                for num in range(100)]
+            )
         source = "\n\n".join(functions)
         print(f"Starting {test_name} speed test")
 
@@ -122,6 +127,12 @@ class TestNewPyc(unittest.TestCase):
         for i in range(100):
             body.append(f"    a{i+1}, b{i+1} = b{i}, a{i}")
         self.do_test_speed('\n'.join(body), "many_locals")
+
+    def test_speed_many_locals_with_call(self):
+        body = ["    a0, b0 = 1, 1"]
+        for i in range(100):
+            body.append(f"    a{i+1}, b{i+1} = b{i}, a{i}")
+        self.do_test_speed('\n'.join(body), "many_locals", call=True)
 
 if __name__ == "__main__":
     unittest.main()
