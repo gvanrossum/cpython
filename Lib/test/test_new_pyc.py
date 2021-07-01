4cafb9df31d68744d9d561a0458d22f9e8fea7ab
Guido van Rossum <guido@python.org>
2021-06-30 21:26:38 -0700

    Revert "remove indices from functions"
    This reverts commit 42c44a0ac7d6c7d50f6441a857999de527cb740d.

--- Lib/test/test_new_pyc.py
+++ Lib/test/test_new_pyc.py
@@ -76,9 +76,14 @@ class TestNewPyc(unittest.TestCase):
 
     def do_test_speed(self, body, test_name, call=False):
         functions = [
-            f"def f(a, b):\n{body}%s" % (f"\nf(0,0)\n" if call else "")
+            f"def f{num}(a, b):\n{body}"
             for num in range(100)
         ]
+        if call:
+            functions.extend([
+                f"\nf{num}({num}, {num+1})\n"
+                for num in range(100)]
+            )
         source = "\n\n".join(functions)
         print(f"Starting {test_name} speed test")
 
